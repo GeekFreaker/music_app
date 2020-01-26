255baac5f819aad489fb833cdd95f36bb63da526
Gabriel Matabane <gabriel@smokeci.com>
2020-01-26 13:05:11 +0200

    Preliminary changes, not submission ready.

---
+++ app/src/main/java/android/example/music_app/MainActivity.java
@@ -0,0 +1,20 @@
+package android.example.music_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //create two albums and all the information for that album
+
+        //Ensure you add these albums to the UI in the scrollView
+
+
+    }
+}
